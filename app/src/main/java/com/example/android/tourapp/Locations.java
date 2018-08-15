83fea7ced3c52b664bcace6087a1255e05f7a6dd
curiousclarissa <clarissa.jarem@gmail.com>
2018-08-15 15:43:38 -0700

    all text now uses string resources

--- app/src/main/java/com/example/android/tourapp/Locations.java
+++ app/src/main/java/com/example/android/tourapp/Locations.java
@@ -1,5 +1,7 @@
 package com.example.android.tourapp;
 
+import android.content.Context;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,17 +12,58 @@ import java.util.Arrays;
  */
 
 public final class Locations {
+    Context context;
     public static final Location[] LOCATIONS = {
-            new Location(App.getResources().getString(R.string.location_u_heights), "206-527-4278", "www.uheightscenter.org", "address", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_PARKSANDREC))),
-            new Location("Farmers Market", "206-632-5234", "https://seatlefarmersmarkets.org/markets/u-district", "address", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_RESTAURANT))),
-            new Location("Taste of India", "206-528-1575", "www.tasteofindiaseattle.com", "address", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_RESTAURANT))),
-            new Location("UW Quad", "206-543-2100", "www.washington.edu", "Pierce Ln", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
-            new Location("University Branch Library", "206-684-4063", "www.spl.org/hours-and-locations/university-branch", "5009 Roosevelt Way NE", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
-            new Location("University Playground", "206-684-4075", "www.seattle.gov/parks/find/parks/university-playground", "4745 9th Ave NE", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_PARKSANDREC))),
-            new Location("Burke Museum", "206-543-7907", "www.burkemuseum.org/", "1413 NE 45th St", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
-            new Location("Hotel Deca", "none: closed", "none: closed", "4507 Brooklyn Ave NE", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_ARCHITECTURE))),
-            new Location("Cowen Park", "206-684-4075", "www.seattle.gov/parks/find/parks/cowen-park", "5849 15th Ave NE", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_PARKSANDREC))),
-            new Location("The Neptune Theatre", "206-682-1414", "www.stgpresents.org/neptune", "1303 NE 45th St", R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
+            new Location(App.getContext().getResources().getString(R.string.location_u_heights),
+                    App.getContext().getResources().getString(R.string.location_u_heights_phone),
+                    App.getContext().getResources().getString(R.string.location_u_heights_url),
+                    App.getContext().getResources().getString(R.string.location_u_heights_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_PARKSANDREC))),
+            new Location(App.getContext().getResources().getString(R.string.location_farmers_market),
+                    App.getContext().getResources().getString(R.string.location_farmers_market_phone),
+                    App.getContext().getResources().getString(R.string.location_farmers_market_url),
+                    App.getContext().getResources().getString(R.string.location_farmers_market_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_RESTAURANT))),
+            new Location(App.getContext().getResources().getString(R.string.location_taste_of_india),
+                    App.getContext().getResources().getString(R.string.location_taste_of_india_phone),
+                    App.getContext().getResources().getString(R.string.location_taste_of_india_url),
+                    App.getContext().getResources().getString(R.string.location_taste_of_india_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_RESTAURANT))),
+            new Location(App.getContext().getResources().getString(R.string.location_uw_quad),
+                    App.getContext().getResources().getString(R.string.location_uw_quad_phone),
+                    App.getContext().getResources().getString(R.string.location_uw_quad_url),
+                    App.getContext().getResources().getString(R.string.location_uw_quad_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
+            new Location(App.getContext().getResources().getString(R.string.location_university_branch_library),
+                    App.getContext().getResources().getString(R.string.location_university_branch_library_phone),
+                    App.getContext().getResources().getString(R.string.location_university_branch_library_url),
+                    App.getContext().getResources().getString(R.string.location_university_branch_library_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
+            new Location(App.getContext().getResources().getString(R.string.location_university_playground),
+                    App.getContext().getResources().getString(R.string.location_seattle_parks_phone),
+                    App.getContext().getResources().getString(R.string.location_university_playground_url),
+                    App.getContext().getResources().getString(R.string.location_university_playground_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_PARKSANDREC))),
+            new Location(App.getContext().getResources().getString(R.string.location_burke_museum),
+                    App.getContext().getResources().getString(R.string.location_burke_museum_phone),
+                    App.getContext().getResources().getString(R.string.location_burke_museum_url),
+                    App.getContext().getResources().getString(R.string.location_burke_museum_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
+            new Location(App.getContext().getResources().getString(R.string.location_hotel_deca),
+                    App.getContext().getResources().getString(R.string.location_data_none_closed),
+                    App.getContext().getResources().getString(R.string.location_data_none_closed),
+                    App.getContext().getResources().getString(R.string.location_hotel_deca_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_ARCHITECTURE))),
+            new Location(App.getContext().getResources().getString(R.string.location_cowen_park),
+                    App.getContext().getResources().getString(R.string.location_seattle_parks_phone),
+                    App.getContext().getResources().getString(R.string.location_cowen_park_url),
+                    App.getContext().getResources().getString(R.string.location_cowen_park_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_PARKSANDREC))),
+            new Location(App.getContext().getResources().getString(R.string.location_neptune_theatre),
+                    App.getContext().getResources().getString(R.string.location_neptune_theatre_phone),
+                    App.getContext().getResources().getString(R.string.location_neptune_theatre_url),
+                    App.getContext().getResources().getString(R.string.location_neptune_theatre_address),
+                    R.drawable.ic_place_black_24dp, new ArrayList<Integer>(Arrays.asList(Location.LOCATION_CATEGORY_POI, Location.LOCATION_CATEGORY_ARCHITECTURE))),
     };
 
     private Locations() {
